be12c8040542a1a166398a1dacf593acaf31b0dc
Brent Greeff <brentgreeff@brent-greeffs-macbook.local>
2009-12-07 14:26:18 +0000

    Fixed pluralization issues (maybe)

--- lib/hash_factory_builder.rb
+++ lib/hash_factory_builder.rb
@@ -1,6 +1,6 @@
 module HashFactoryBuilder
   def build(class_name, options)
-    new_obj = class_name.to_s.classify.constantize.new
+    new_obj = class_name.to_s.pluralize.classify.constantize.new
     new_obj.send :attributes=, options, false
     new_obj.save!
     
